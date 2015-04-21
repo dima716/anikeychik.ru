a71340f4fc0de8cdf3905b98793c885eca59cae5
Anikeychik Dmitry <Dima716@yandex.ru>
2015-04-21 12:35:58 +0300

    Filled Resume section with my info

--- levels/blocks/skills/skills.js
+++ levels/blocks/skills/skills.js
@@ -9,48 +9,69 @@ var options = {
     animateScale: true
 };
 var data = {
-    html_css: [{
-        value: 50,
-        color: "#404148"
-    }, {
-        value: 50,
-        color: "#fff"
-    }],
-    sass: [{
-        value: 90,
-        color: "#404148"
-    }, {
-        value: 10,
-        color: "#fff"
-    }],
-    jquery: [{
-        value: 90,
-        color: "#404148"
-    }, {
-        value: 10,
-        color: "#fff"
-    }],
-    rails: [{
-        value: 60,
-        color: "#404148"
-    }, {
-        value: 40,
-        color: "#fff"
-    }],
-    backbone: [{
-        value: 50,
-        color: "#404148"
-    }, {
-        value: 50,
-        color: "#fff"
-    }],
-    photoshop: [{
-        value: 75,
-        color: "#404148"
-    }, {
-        value: 25,
-        color: "#fff"
-    }]
+  html_css: [{
+    value: 85,
+    color: "#404148"
+  }, {
+    value: 15,
+    color: "#fff"
+  }],
+  javascript: [{
+    value: 65,
+    color: "#404148"
+  }, {
+    value: 35,
+    color: "#fff"
+  }],
+  jquery: [{
+    value: 75,
+    color: "#404148"
+  }, {
+    value: 25,
+    color: "#fff"
+  }],
+  bem: [{
+    value: 45,
+    color: "#404148"
+  }, {
+    value: 55,
+    color: "#fff"
+  }],
+  gulp: [{
+    value: 70,
+    color: "#404148"
+  }, {
+    value: 30,
+    color: "#fff"
+  }],
+  photoshop: [{
+    value: 50,
+    color: "#404148"
+  }, {
+    value: 50,
+    color: "#fff"
+  }],
+  php: [{
+    value: 40,
+    color: "#404148"
+  }, {
+    value: 60,
+    color: "#fff"
+  }],
+  mysql: [{
+    value: 40,
+    color: "#404148"
+  }, {
+    value: 60,
+    color: "#fff"
+  }],
+  linux: [{
+    value: 40,
+    color: "#404148"
+  }, {
+    value: 60,
+    color: "#fff"
+  }]
 };
 
 var showSkills = function () {
